4e1cf3f2eb8e35d19ec6f716b682a97c799211d9
Edwin P. Magezi <emagezi@gmail.com>
2017-03-31 12:17:47 +0300

    Show blocking progress when reloading

--- src/PullToRefresh/widget/PullToRefresh.ts
+++ src/PullToRefresh/widget/PullToRefresh.ts
@@ -4,7 +4,6 @@ import * as domConstruct from "dojo/dom-construct";
 import * as WidgetBase from "mxui/widget/_WidgetBase";
 
 import { PullToRefresh } from "./handlers/PullToRefresh";
-
 import "./ui/PullToRefresh.css";
 
 class PullToRefreshWrapper extends WidgetBase {
@@ -14,18 +13,44 @@ class PullToRefreshWrapper extends WidgetBase {
 
     private pullToRefreshElement: HTMLElement;
     private pullToRefresh: PullToRefresh;
+    private progressId: number;
 
     postCreate() {
-        // We share the refresh element across pages. Else the setup and destroy will conflict
+        this.onRefresh = this.onRefresh.bind(this);
+        this.onSyncFailure = this.onSyncFailure.bind(this);
+
+        if (!window.mx.data.synchronizeOffline && !window.mx.data.synchronizeDataWithFiles) {
+            domConstruct.create("div", {
+                class: "alert alert-danger",
+                innerHTML: "The pull to refresh widget is not compatible with this version of Mendix"
+            }, this.domNode);
+        } else {
+            // We share the refresh element across pages. Else the setup and destroy will conflict
+            this.setUpWidgetDom();
+            this.pullToRefresh.setupEvents();
+        }
+    }
+
+    uninitialize(): boolean {
+        this.pullToRefresh.removeEvents();
+
+        return true;
+    }
+
+    private setUpWidgetDom() {
         this.pullToRefreshElement = dojoDom.byId("widget-pull-to-refresh");
         if (!this.pullToRefreshElement) {
             this.pullToRefreshElement = domConstruct.create("div", {
                 class: "pull-to-refresh-pull-to-refresh",
                 id: "widget-pull-to-refresh",
-                innerHTML: `<div class='pull-to-refresh-box'><div class='pull-to-refresh-content'>
-                    <div class='pull-to-refresh-icon'></div>
-                    <div class='pull-to-refresh-text'></div>
-                </div></div>`
+                innerHTML: `
+                    <div class='pull-to-refresh-box'>
+                        <div class='pull-to-refresh-content'>
+                            <div class='pull-to-refresh-icon'></div>
+                            <div class='pull-to-refresh-text'></div>
+                        </div>
+                    </div>
+                `
             }, document.body, "first");
         }
 
@@ -37,21 +62,35 @@ class PullToRefreshWrapper extends WidgetBase {
             refreshText: this.refreshText,
             releaseToRefreshText: this.releaseToRefreshText
         });
-
-        this.pullToRefresh.setupEvents();
     }
 
-    uninitialize(): boolean {
-        this.pullToRefresh.removeEvents();
-
-        return true;
-    }
-
+    // Note; this function is hooking into the Mendix private API, this is subject to change without notice!
+    // Please do not re-use this. The only supported API is publicly documented at
+    // https://apidocs.mendix.com/7/client/
     private onRefresh(callback: () => void) {
-        window.mx.data.synchronizeDataWithFiles(() => window.mx.ui.reload(callback), (error) => {
-            logger.error(error);
-            window.mx.ui.info(window.mx.ui.translate("mxui.sys.UI", "sync_error"), true);
+        this.progressId = window.mx.ui.showProgress(null, true);
+        this.pullToRefresh.removeEvents();
+        if (window.mx.data.synchronizeOffline) {
+            window.mx.data.synchronizeOffline({ fast: false }, () => this.onSyncSuccess(callback), this.onSyncFailure);
+        } else if (window.mx.data.synchronizeDataWithFiles) {
+            window.mx.data.synchronizeDataWithFiles(() => this.onSyncSuccess(callback), this.onSyncFailure);
+        }
+    }
+
+    private onSyncSuccess(callback: () => void) {
+        window.mx.ui.reload(() => {
+            if (this.progressId) {
+                window.mx.ui.hideProgress(this.progressId);
+            }
+            this.pullToRefresh.setupEvents();
+            if (callback) callback();
         });
+    }
+
+    private onSyncFailure() {
+        if (this.progressId) window.mx.ui.hideProgress(this.progressId);
+        window.mx.ui.info(window.mx.ui.translate("mxui.sys.UI", "sync_error"), true);
+        this.pullToRefresh.setupEvents();
     }
 }
 
